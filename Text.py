6cd10c25f68db80f31a70fd2d91a48fb2e0e6c62
Blade Nelson <powderblock@gmail.com>
2014-08-08 05:15:19 -0400

    Removed "@" in SMS Gateway User Input
    Removed the need to have "@" before the SMS Gateway, fixed the spacing
    in the first two prompts.

--- Text.py
+++ Text.py
@@ -9,8 +9,8 @@ server.starttls()
 print(">Setting up account information.")
 
 #Get user account information
-account = str(raw_input("Enter Google Talk account username: (Example: username123)"))
-password = str(raw_input("Enter Google Talk account password: (Example: abc123)"))
+account = str(raw_input("Enter Google Talk account username: (Example: username123) "))
+password = str(raw_input("Enter Google Talk account password: (Example: abc123) "))
 server.login(account+'@gmail.com', password)
 
 print(">Setting up message to be sent.")
@@ -18,11 +18,11 @@ print(">Setting up message to be sent.")
 number = str(raw_input("Enter a number to text. (Example: 1234567890) "))
 
 #Get SMS gateway:
-gateway = str(raw_input("Enter SMS Gateway domain. (AT&T: @txt.att.net, T-Mobile: @tmomail.net) "))
+gateway = str(raw_input("Enter SMS Gateway domain. (AT&T: txt.att.net, T-Mobile: tmomail.net) "))
 
 #Get message from user:
 message = str(raw_input("Enter a message: "))
 
 #Send message
-server.sendmail('', number+gateway, message)
+server.sendmail('', number+'@'+gateway, message)
 print(">Message sent!")
